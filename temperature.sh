3beb4f9aa28d0ef4e9758d218e436916504ac016
huangcr <huangcr@ksu.edu>
2019-09-20 22:03:51 -0500

    F to K

--- temperature.sh
+++ temperature.sh
@@ -7,4 +7,6 @@ read temp
 
 temp_c=$(echo "scale=2;(5/9)*($temp-32)" |bc)
 echo "$temp degrees F = $temp_c degrees C"
+temp_k=$(echo "scale=2;(5/9)*($temp-32)+273.15" |bc)
+echo "$temp degrees F = $temp_k degrees K"
 exit 1
